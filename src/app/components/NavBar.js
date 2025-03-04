d6494dcd4213445299b5b10a93e062ef2afbf634
snaramirez872 <seanramirez818@gmail.com>
2025-03-03 22:51:26 -0500

    added navbar, icons, and resume link

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export function NavBar() {
+    const resumeLink = process.env.NEXT_PUBLIC_RESUME_LINK;
+    return (
+        <nav className='navbar'>
+            <div className='nav-container'>
+                <Link href="/" className='logo'>Sean A Ramirez</Link>
+                <div className='links'>
+                    <Link href='/' className='nav-link'>About Me</Link>
+                    <Link href='/projects' className='nav-link'>Projects</Link>
+                    <Link href={resumeLink} className='resume-link' target='_blank' rel='noreferrer'>Resume</Link>
+                </div>
+            </div>
+        </nav>
+    );
+}
---
+++ src/app/projects/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className='projects'>
+            <p>list of projects here</p>
+        </div>
+    );
+}
