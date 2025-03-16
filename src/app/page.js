6f2a414f53dd4cc99267bcb1f1434e2ab72c9b20
snaramirez872 <seanramirez818@gmail.com>
2025-03-16 19:44:11 -0400

    added info about my photography in the about section

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,8 @@ export default function Home() {
       <div className='words'>
         <p className='intro'>I&apos;m a <span className='keywords'>Software Developer</span> specializing in <span className='keywords'>Front End Development/Design</span> using <span className='keywords'>Next.js</span>, <span className='keywords'>React.js</span>, and <span className='keywords'>CSS</span>.</p>
         <p className='extras'>When I&apos;m not coding I enjoy playing video games, photography, watching movies, and watching anime!</p>
-        <p className='intro-to-projs'>I&apos;ve been making web applications for a couple of years now and they can be viewed by <span className='desktop-mode'>clicking &quot;Projects&quot; at the top right</span><span className='mobile-mode'>tapping &quot;Projects&quot; below</span>.</p>
+        <p className='photo'>Photography has been a hobby of mine for many years! All of my work can be viewed on my Instagram by <span className='desktop-mode'>clicking the Instagram icon to the right</span><span className='mobile-mode'>tapping the Instagram icon at the bottom of this page</span>.</p>
+        <p className='intro-to-projs'>I&apos;ve been making web applications for a few years and they can be viewed by <span className='desktop-mode'>clicking &quot;Projects&quot; at the top right</span><span className='mobile-mode'>tapping &quot;Projects&quot; below</span>.</p>
         <p className='right-now'>Right now, I&apos;m doing the writing for <span className="keywords">SNAR-Dev</span> and I&apos;m also developing <span className="keywords">Effectiveness Dex</span>. More info can be found in the &quot;Projects&quot; tab.</p>
       </div>
       <div className='mobile-tabs'>
