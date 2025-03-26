c52118b62bbad197aaed3d2a52e3f8ff0d2ba9d8
snaramirez872 <seanramirez818@gmail.com>
2025-03-25 20:11:03 -0400

    edited homepage info

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,10 @@ export default function Home() {
     <>
       <div className='homepage-desktop'>
         <div className='words'>
-          <p className='intro'>I&apos;m a <span className='keywords'>Software Developer</span> specializing in <span className='keywords'>Front End Development/Design</span> using <span className='keywords'>Next.js</span>, <span className='keywords'>React.js</span>, and <span className='keywords'>CSS</span>.</p>
-          <p className='extras'>When I&apos;m not coding I enjoy playing video games, photography, watching movies, and watching anime!</p>
-          <p className='photo'>Photography has been a hobby of mine for many years! All of my work can be viewed on my Instagram by clicking the Instagram icon to the right.</p>
-          <p className='intro-to-projs'>I&apos;ve been making web applications for a few years and they can be viewed by clicking &quot;Projects&quot; at the top right.</p>
-          <p className='right-now'>Right now, I&apos;m doing the writing for <span className="keywords">SNAR-Dev</span> and I&apos;m also developing <span className="keywords">Effectiveness Dex</span>. More info can be found in the &quot;Projects&quot; tab.</p>
+          <p className='intro'>Hello! I&apos;m a <span className='keywords'>Software Developer</span> based in New Jersey. I prioritize <span className='keywords'>Front End Web Development</span>, but I also have some experience with <span className='keywords'>Full Stack Development</span>.</p>
+          <p className='extras'>When I&apos;m not coding or reading some technical documents, I'm usually playing video games or watching <i>something</i> on YouTube or Netflix.</p>
+          <p className='photo'>Photography has also been a hobby of mine for many years! All of my work can be viewed on my Instagram by clicking the Instagram icon to the right.</p>
+          <p className='right-now'>Right now, I&apos;m doing the writing for <span className="keywords">SNAR-Dev</span> and I&apos;m also developing <span className="keywords">Effectiveness Dex</span>. More info about these works and my other projects can be found in the &quot;Projects&quot; tab.</p>
         </div>
         <div className='profile-card'>
           <Image className='profile-picture' src={ProfilePic} alt='profile picture' height='170' width='170' />
