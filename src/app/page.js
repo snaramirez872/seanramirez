9a783d74c80e85450d0e9576754e7cd592524a35
snaramirez872 <seanramirez818@gmail.com>
2025-07-02 20:11:45 -0400

    edited homepage

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <div className='homepage-desktop'>
         <div className='words'>
           <p className='intro'>Hello! I&apos;m a <span className='keywords'>Software Developer</span> based in New Jersey. I prioritize <span className='keywords'>Front End Web Development</span>, but I also have some experience with <span className='keywords'>Full Stack Development</span>.</p>
-          <p className='right-now'>Right now I&apos;m enrolled in a <span className='keywords'>Medical Billing and Coding</span> online course at NJCU. I am also continuing to brush up on <span className="keywords">Website Development</span> through small-scale projects.</p>
+          <p className='right-now'>Right now I&apos;m pursuing a Software Engineering Internship with <a className='tools' href='https://www.withsentari.com/'>Sentari AI</a>. I am also currently enrolled in a <span className='keywords'>Medical Billing and Coding</span> online course at NJCU.</p>
         </div>
         <div className='profile-card'>
           <Image className='profile-picture' src={ProfilePic} alt='profile picture' height='170' width='170' />
