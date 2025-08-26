9740f297a81995c3630fd1f5d50dfe20ba49e5ca
snaramirez872 <seanramirez818@gmail.com>
2025-08-25 20:33:32 -0400

    changed email address

--- src/app/components/Icons.js
+++ src/app/components/Icons.js
@@ -8,7 +8,7 @@ import './css/icons.css';
 export function Icons() {
     return (
         <div className='icons'>
-            <Link href='mailto:snaramirez872@gmail.com' target='_blank' rel='noreferrer'><Image src={email} alt='' height='40' width='40' /></Link>
+            <Link href='mailto:sean.ramirez.dev@gmail.com' target='_blank' rel='noreferrer'><Image src={email} alt='' height='40' width='40' /></Link>
             <Link href='https://github.com/snaramirez872' target='_blank' rel='noreferrer'><Image src={github} alt='' height='40' width='40' /></Link>
             <Link href='https://linkedin.com/in/seanaramirez' target='_blank' rel='noreferrer'><Image src={linkedin} alt='' height='40' width='40' /></Link>
         </div>
