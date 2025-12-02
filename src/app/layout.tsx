ecc4ba2f4757c288afc923c0fd8c8d028721dd6e
snaramirez872 <seanramirez818@gmail.com>
2025-12-02 18:02:38 -0500

    restructured metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,30 @@ import "./globals.css";
 import { Footer } from "./components/Footer";
 import { Analytics } from '@vercel/analytics/next';
 import type { ReactNode } from "react";
+import { NavBar } from "./components/NavBar";
 
 export const metadata = {
-  title: "Hello, World! My Name is Sean A Ramirez",
-  description: "I am a web developer who has made various projects. This is my portfolio.",
+  title: "Sean A Ramirez | Web Developer & Software Engineer | JavaScript, HTML, CSS, React.js, Next.js, Python, WordPress",
+  description: "Portfolio of Sean A Ramirez, a web developer specialzing in JavaScript, HTML, CSS, React.js, Next.js, and WordPress, showcasing modern and responsive websites and front-end projects.",
+  metadataBase: new URL("https://seanramirez.vercel.app"), // temp url while waiting for domain
+  openGraph: {
+    title: "Sean A Ramirez | Web Developer & Software Engineer | JavaScript, HTML, CSS, React.js, Next.js, Python, WordPress",
+    description: "Portfolio of Sean A Ramirez, a web developer specialzing in JavaScript, HTML, CSS, React.js, Next.js, and WordPress, showcasing modern and responsive websites and front-end projects.",
+    images: [
+      {
+        url: "/profile.png",
+        width: 1200,
+        height: 630,
+        alt: "Featured image for the portfolio of Sean A. Ramirez",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sean A Ramirez | Web Developer & Software Engineer | JavaScript, HTML, CSS, React.js, Next.js, Python, WordPress",
+    description: "Portfolio of Sean A Ramirez, a web developer specialzing in JavaScript, HTML, CSS, React.js, Next.js, and WordPress, showcasing modern and responsive websites and front-end projects.",
+    images: ["/profile.png"],
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NavBar />
         {children}
         <Footer />
         <Analytics />
