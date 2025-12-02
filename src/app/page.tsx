c44175ffda620f37598650e149c1363c1cd5aa29
snaramirez872 <seanramirez818@gmail.com>
2025-12-02 18:02:54 -0500

    reworded home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className='homepage-desktop'>
         <div className='words'>
           <p className='intro'>Hello! I&apos;m a <span className='keywords'>Software Developer</span> based in New Jersey. I prioritize <span className='keywords'>Front End Web Development</span>, but I also have some experience with <span className='keywords'>Full Stack Development</span>.</p>
-          <p className='right-now'>I&apos;m currently engaging in different opportunites, but I&apos;m always happy to connect with people in the industry! My email and LinkedIn are both available to the right.</p>
+          <p className='right-now'>I&apos;m always happy to connect with people in the industry! My email and LinkedIn are both available to the right.</p>
         </div>
         <div className='profile-card'>
           <Image className='profile-picture' src={ProfilePic} alt='profile picture' height='170' width='170' />
