949c9753f2a53f8800395c06458a3af0800abb72
snaramirez872 <seanramirez818@gmail.com>
2025-12-12 19:11:29 -0500

    installed speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Footer } from "./components/Footer";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { ReactNode } from "react";
 import { NavBar } from "./components/NavBar";
 
@@ -43,6 +44,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
