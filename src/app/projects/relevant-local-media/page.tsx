82608af62661e7b0b59886b15defeb27cf414dc8
snaramirez872 <sean.ramirez.dev@gmail.com>
2026-05-27 19:41:42 -0400

    added back the locations of each RLM client site

--- src/app/projects/relevant-local-media/page.tsx
+++ src/app/projects/relevant-local-media/page.tsx
@@ -12,154 +12,161 @@ export default function RLMPage() {
                 <section className="professional-projects">
                     <section className="personal-grid">
                         <RLMProjCard
-                            title="Blue Jeans Pizza, Pasta, and Wings"
+                            title="Aced Enterprises, Inc. - Blackwood, NJ"
+                            url="https://acedenterprise.wpenginepowered.com/"
+                            techList="WordPress, Elementor, CSS"
+                        >
+                            <p className="description">This website is not yet live as of May 27th, 2026.</p>
+                        </RLMProjCard>
+                        <RLMProjCard
+                            title="Blue Jeans Pizza, Pasta, and Wings - Blue Ridge, GA"
                             url="https://bluejeansga.wpenginepowered.com/"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website is not yet live as of May 15th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Garson"
+                            title="Garson - Houston, TX"
                             url="https://garsonshouston.wpenginepowered.com/"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website is not yet live as of May 9th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Reali's Cuisine"
-                            url="https://realiscuisine.wpenginepowered.com/"
+                            title="Reali's Cuisine - Johnston, RI"
+                            url="https://realis-cuisine.com/"
                             techList="WordPress, Elementor, CSS"
                         >
-                            <p className="description">This website is not yet live as of May 9th, 2026.</p>
+                            <p className="description">This website is live as of May 27th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Lexi J's Boutique"
+                            title="Lexi J's Boutique - Fort Myers, FL"
                             url="https://lexijs.com"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website went live on May 8th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Banjo's BBQ"
-                            url="https://eigthstreetpt.wpenginepowered.com"
+                            title="Banjo's BBQ - Dayton, TN"
+                            url="https://banjosbbq.com/"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website went live on April 27th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Charleston Movement Co. Physical Therapy"
+                            title="Charleston Movement Co. Physical Therapy - Charlston, SC"
                             url="https://eigthstreetpt.wpenginepowered.com"
                             techList="WordPress, Elementor, CSS, Da Vinci Resolve"
                         >
                             <p className="description">This website is not yet live as of April 9th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Charleston Movement Co. Pilates"
+                            title="Charleston Movement Co. Pilates - Charleston, SC"
                             url="https://eighthpilates.wpenginepowered.com"
                             techList="WordPress, Elementor, CSS, Da Vinci Resolve"
                         >
                             <p className="description">This website is not yet live as of April 9th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="US BanCard Merchant Services"
+                            title="US BanCard Merchant Services - Brentwood, TN"
                             url="https://usbancard.com"
                             techList="WordPress, Elementor, CSS, Da Vinci Resolve"
                         >
                             <p className="description">This website went live on March 23rd, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Pine Island Seafood Co."
+                            title="Pine Island Seafood Co. - Matlacha, FL"
                             url="https://pineislandseafoodco.com"
                             techList="WordPress, Elementor, CSS, Canva"
                         >
                             <p className="description">This website went live on April 28th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Old Fort"
+                            title="Old Fort - Cleveland, TN"
                             url="https://oldforttn.com"
                             techList="WordPress, Elementor, CSS, Canva"
                         >
                             <p className="description">This website went live on March 4th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="New York Bagel Bakery"
+                            title="New York Bagel Bakery - Philadelphia, PA"
                             url="https://newyorkbagelspa.com"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website went live on April 27th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Skeeter's Pub"
+                            title="Skeeter's Pub - Blackwood, NJ"
                             url="https://skeeterspub.com"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website went live on February 20th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Ali's Roti Shop"
+                            title="Ali's Roti Shop - Plantation, FL"
                             url="https://alisrotifl.com"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website went live on March 18th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Rexy's Restaurant & Bar"
+                            title="Rexy's Restaurant & Bar - Haddon Township, NJ"
                             url="https://rexys.com"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website went live in February 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Carolina Carpet Cleaning of the Midlands, Inc."
+                            title="Carolina Carpet Cleaning of the Midlands, Inc. - West Columbia, SC"
                             url="https://carolinacarpetcleaning.com"
                             techList="WordPress, Elementor, CSS, Canva"
                         >
                             <p className="description">This website went live on January 20th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Old Village Pizza"
+                            title="Old Village Pizza - Westampton, NJ"
                             url="https://oldvillagepizzanj.com"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website went live on January 16th, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="JP McGurkee's Sandwich Shop"
+                            title="JP McGurkee's Sandwich Shop - Pheonix, AZ"
                             url="https://jpmcgurkees.wpenginepowered.com"
                             techList="WordPress, Elementor, CSS, Canva"
                         >
                             <p className="description">This website is not yet live as of December 17th, 2025.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="The Sweet Sage Cafe & Boutique"
+                            title="The Sweet Sage Cafe & Boutique - North Redington Beach, FL"
                             url="https://sweetsagecafe.com"
                             techList="WordPress, Elementor, CSS, Canva"
                         >
                             <p className="description">This website went live on January 2nd, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="El Chullo Peruvian Restaurant"
+                            title="El Chullo Peruvian Restaurant - Pheonix, AZ"
                             url="https://elchullo.wpenginepowered.com"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website is not yet live as of November 26th, 2025.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="The Swiss Bar"
+                            title="The Swiss Bar - Reno, NV"
                             url="https://swissbarreno.com"
                             techList="WordPress, Elementor, CSS, Canva"
                         >
                             <p className="description">This website went live on December 18th, 2025.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="4th Street Pizza"
+                            title="4th Street Pizza - St. Petersburg, FL"
                             url="https://4thstreetpizzas.com"
                             techList="WordPress, Elementor, CSS"
                         >
                             <p className="description">This website went live on January 21st, 2026.</p>
                         </RLMProjCard>
                         <RLMProjCard
-                            title="Quick and Quality"
+                            title="Quick and Quality - Philadelphia, PA"
                             url="https://quickandqualitydeli.com"
                             techList="WordPress, Elementor, CSS, Canva"
                         >
